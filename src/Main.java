ba16f671b934a08c02279e8ef6e4beea05583aad
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-26 10:07:00 -0500

    commit - Main / .

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import oracle.jdbc.proxy.annotation.Pre;
+
 import java.sql.*;
 
 public class Main {
@@ -32,14 +34,57 @@ public class Main {
             }
         }
     }
-
-    private static void mostrarProductos(Connection conn) {
-    }
-
-    private static void actualizarProducto(Connection conn, int i, String cuadernoProfesional, int i1, int i2) {
-        
-    }
-
     // metodo para llamar al procedimiento almacenado .
-    
+    private static void actualizarProducto(Connection conn, int id, String nombre, double precio, int stock) {
+        try {
+            CallableStatement cs =  conn.prepareCall("{ call ACTUALIZAR_PRODUCTO(?, ?, ?, ?)}");
+
+            cs.setInt(1, id);
+            cs.setString(2, nombre);
+            cs.setDouble(3, precio);
+            cs.setInt(4, stock);
+
+            cs.execute();
+
+            System.out.println("\n🔄 Producto actualizado correctamente:");
+            System.out.println("ID: " + id);
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Precio: " + precio);
+            System.out.println("Stock: " + stock);
+
+            cs.close();
+        } catch (Exception e) {
+            System.out.println("❌ Error al actualizar producto");
+            e.printStackTrace();
+        }
+    }
+
+    // Método para consultar e imprimir todos los registros .
+    private static void mostrarProductos(Connection conn) {
+        try {
+            String sql = "SELECT *  FROM PRODUCTOS_PAPELERIA ORDER BY ID";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            System.out.println("\n📋 LISTADO DE PRODUCTOS:");
+            System.out.println("--------------------------------------------------");
+
+            while (rs.next()) {
+                System.out.println(
+                        "ID: " + rs.getInt("ID") +
+                                " | Nombre: " + rs.getString("NOMBRE") +
+                                " | Precio: " + rs.getDouble("PRECIO") +
+                                " | Stock: " + rs.getInt("STOCK")
+                );
+            }
+
+            System.out.println("--------------------------------------------------");
+
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("❌ Error al consultar productos");
+            e.printStackTrace();
+        }
+    }   
 }
